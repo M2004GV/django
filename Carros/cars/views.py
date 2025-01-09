2c77ffd4b3529c275af9ca49804b479332af2684
Mateus Garcia de Souza Vieira <garciamateus285@gmail.com>
2025-01-08 21:17:32 -0300

    adicionando a tela cars ao sistema

--- Carros/cars/views.py
+++ Carros/cars/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def cars_view(request):
-    return HttpResponse('Meus Carros')
+def home_view(request):
+    return render(request, 'cars.html')
