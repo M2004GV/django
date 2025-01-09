e581235959ff976427860404bd157837945dad32
Mateus Garcia de Souza Vieira <garciamateus285@gmail.com>
2025-01-08 21:36:24 -0300

    listando todos os objetos do banco de dados

--- Carros/cars/views.py
+++ Carros/cars/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from cars.models import Car
 
 def home_view(request):
+    cars = Car.objects.all()#selecionar todos os objetos do banco de dados Car
+    #print(cars)
+
     return render(
         request,          
         'cars.html', {
-        'cars':{'model':'Astra 2.0'}}
+        'cars': cars}
     )
