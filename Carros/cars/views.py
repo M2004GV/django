e7eb1dbe58a5c5732458aa4177bd1c52cc09c484
Mateus Garcia de Souza Vieira <garciamateus285@gmail.com>
2025-01-09 21:36:27 -0300

    lindando com o request

--- Carros/cars/views.py
+++ Carros/cars/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from cars.models import Car
 
 def home_view(request):
-    #cars = Car.objects.all() #selecionar todos os objetos do banco de dados Car
+    cars = Car.objects.all() #selecionar todos os objetos do banco de dados Car
     #print(cars)
 
-    cars = Car.objects.filter(brand__name='Fiat')
+    #cars = Car.objects.filter(brand__name='Fiat')
+
+    #cars = Car.objects.filter(model__contains='Chevette')
 
 
     return render(
