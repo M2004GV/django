2c12cd75eb58781435f5ad451552577d85b49b9e
Mateus Garcia de Souza Vieira <garciamateus285@gmail.com>
2025-01-09 21:24:29 -0300

    explorando funções do ORM do django(filtros)

--- Carros/cars/views.py
+++ Carros/cars/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from cars.models import Car
 
 def home_view(request):
-    cars = Car.objects.all()#selecionar todos os objetos do banco de dados Car
+    #cars = Car.objects.all() #selecionar todos os objetos do banco de dados Car
     #print(cars)
+
+    cars = Car.objects.filter(brand__name='Fiat')
+
 
     return render(
         request,          
