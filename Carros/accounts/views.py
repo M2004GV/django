337c8d807651b9e945bb14761c05cff9df4644a0
Mateus Garcia de Souza Vieira <garciamateus285@gmail.com>
2025-01-14 21:01:38 -0300

    criando a página de criação de usuário

--- Carros/accounts/views.py
+++ Carros/accounts/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
-def register_view():
-    ...
+def register_view(request):
+    user_forms = UserCreationForm()
+    return render(request, 'register.html', {'user_form': user_forms})
