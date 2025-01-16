e1922b50d353ecbbefe242d21a47d2793bdea007
Mateus Garcia de Souza Vieira <garciamateus285@gmail.com>
2025-01-16 17:57:36 -0300

    refatorando código de function based views para class based views

--- Carros/cars/views.py
+++ Carros/cars/views.py
@@ -1,23 +1,29 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('model') #selecionar todos os objetos do banco de dados Car
-    search = request.GET.get('search')
-    #print(cars)
+class CarsView(View):
+    
+    def get(self, request):
+        cars = Car.objects.all().order_by('model') #selecionar todos os objetos do banco de dados Car
+        search = request.GET.get('search')
+        #cars = Car.objects.filter(brand__name='Fiat')
 
-    #cars = Car.objects.filter(brand__name='Fiat')
+        if search:
+            cars = Car.objects.filter(model__icontains=search)#icontains vai ignorar o uppercase e lowercase
 
-    if search:
-        cars = Car.objects.filter(model__icontains=search)#icontains vai ignorar o uppercase e lowercase
+        return render(
+            request,          
+            'cars.html', {
+            'cars': cars}
+        )
 
-    return render(
-        request,          
-        'cars.html', {
-        'cars': cars}
-    )
+class NewCarView(View):
 
+    def post(self, request):
+        ...
+         
 def new_car_view(request):
     if request.method == 'POST':
         new_car_form = CarModelForm(request.POST, request.FILES)
