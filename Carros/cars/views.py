3cc1a402a855aad5520a1c6fc076d4798c1642bc
root <root@Ma2004.localdomain>
2025-10-05 01:17:17 -0300

    removendo arquivos indesejados do git ignore

--- Carros/cars/views.py
+++ Carros/cars/views.py
@@ -2,18 +2,19 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
   
 class CarsListView(ListView):
     model = Car
     template_name = 'cars.html'
     context_object_name = 'cars'
+    paginate_by = 3                     # Define o número de itens por página
 
     def get_queryset(self):
-        queryset = super().get_queryset().order_by('model')
+        queryset = super().get_queryset().select_related('brand').order_by('model') # Otimiza a consulta para buscar a marca relacionada
         search = self.request.GET.get('search')
         if search:
-            cars = cars.filter(model__icontains=search)
+            queryset = queryset.filter(model__icontains=search) # Filtra os carros pelo modelo, ignorando maiúsculas/minúsculas
 
         return queryset
 
@@ -33,4 +34,7 @@ class CarUpdateView(UpdateView):
     template_name = 'car_update.html'
     success_url = '/cars/'
 
-
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
+    success_url = '/cars/'
